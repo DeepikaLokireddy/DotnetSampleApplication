c14461a5c12b57250f49fcac30352759d473fa05
DeepikaLokireddy <118918810+DeepikaLokireddy@users.noreply.github.com>
2025-05-07 17:19:42 +0530

    Added my posts dotnet app

--- PostsApplication/Controllers/EmployeeController.cs
+++ PostsApplication/Controllers/EmployeeController.cs
@@ -7,15 +7,17 @@ namespace PostsApplication.Controllers
     [Route("[controller]")]
     public class EmployeeController : ControllerBase
     {
-        private static readonly List<Employee> employees = new()
+        private static readonly List<Employee> emp = new()
         {
-            new Employee { Id = 1,Name="john Doe",Designation="Manager",Email="xyz@gmail.com",Phone="42343234234" },
+            new Employee { Id = 1,Name="john Doe2",Designation="Manager",Email="xyz@gmail.com",Phone="42343234234" },
             new Employee { Id = 2,Name="Jack more",Designation="Senior Manager",Email="abc@gmail.com",Phone="121212333" }
         };
         [HttpGet]
         public IActionResult GetEmployeeDetails()
         {
-            return Ok(employees);
+            return Ok(emp);
         }
+
+
     }
 }
--- PostsApplication/obj/Debug/net9.0/PostsApplication.AssemblyInfo.cs
+++ PostsApplication/obj/Debug/net9.0/PostsApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PostsApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d18b47c1848a8814218d4494821bfbf21f68c761")]
 [assembly: System.Reflection.AssemblyProductAttribute("PostsApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PostsApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
