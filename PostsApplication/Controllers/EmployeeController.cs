d18b47c1848a8814218d4494821bfbf21f68c761
DeepikaLokireddy <118918810+DeepikaLokireddy@users.noreply.github.com>
2025-05-07 14:45:44 +0530

    Added my posts dotnet app

---
+++ PostsApplication/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PostsApplication.Models;
+
+namespace PostsApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private static readonly List<Employee> employees = new()
+        {
+            new Employee { Id = 1,Name="john Doe",Designation="Manager",Email="xyz@gmail.com",Phone="42343234234" },
+            new Employee { Id = 2,Name="Jack more",Designation="Senior Manager",Email="abc@gmail.com",Phone="121212333" }
+        };
+        [HttpGet]
+        public IActionResult GetEmployeeDetails()
+        {
+            return Ok(employees);
+        }
+    }
+}
---
+++ PostsApplication/Controllers/PostsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PostsApplication.Models;
+
+namespace PostsApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PostsController : ControllerBase
+    {
+
+        private static readonly List<Posts> posts = new()
+        {
+            new Posts {Id = 1,Title="Commerce",Author = "John"},
+            new Posts {Id = 2,Title="History",Author = "Jack"},
+        };
+        [HttpGet]
+        public IActionResult GetPosts()
+        {
+            
+            return Ok(posts);
+        }
+    }
+}
